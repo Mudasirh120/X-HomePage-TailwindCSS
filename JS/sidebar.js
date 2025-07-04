1cf55d19c66e03d9c0d4bffc3968bda020f26289
Mudasirh120 <mudasirh120@gmail.com>
2025-07-04 23:01:45 +0500

    Basic Home Page Created

---
+++ JS/sidebar.js
@@ -0,0 +1,70 @@
+// Sidebar
+const extras = document.querySelectorAll(".extra");
+const extra1 = document.querySelector(".extra-first");
+const extra2 = document.querySelector(".extra-second");
+const extra3 = document.querySelector(".extra-third");
+const extra4 = document.querySelector(".extra-fourth");
+const aside = document.querySelector(".third");
+const sideBar = document.querySelector(".first");
+const sideBarMenu = document.querySelectorAll(".sidebar-text");
+const sideBarBtnIcon = document.querySelector(".sidebar-btn-icon");
+function reloadBar() {
+  const height = window.innerHeight;
+  const width = window.innerWidth;
+  extras.forEach((el) => el.classList.add("not-show"));
+  if (height > 670) {
+    extra1.classList.remove("not-show");
+  }
+  if (height > 720) {
+    extra2.classList.remove("not-show");
+  }
+  if (height > 770) {
+    extra3.classList.remove("not-show");
+  }
+  if (height > 820) {
+    extra4.classList.remove("not-show");
+  }
+  if (width <= 1280) {
+    sideBar.style.width = "20%";
+    sideBarMenu.forEach((el) => el.classList.add("not-show"));
+    sideBarBtnIcon.classList.remove("!hidden");
+  } else {
+    sideBarMenu.forEach((el) => {
+      if (el.classList.contains("not-show")) {
+        el.classList.remove("not-show");
+      }
+    });
+    sideBarBtnIcon.classList.add("!hidden");
+  }
+  if (width <= 1000) {
+    aside.classList.add("not-show");
+  } else {
+    aside.classList.remove("not-show");
+  }
+}
+window.addEventListener("load", reloadBar);
+window.addEventListener("resize", reloadBar);
+// Tweet Box
+const tweet = document.querySelector(".quick-tweet");
+const tweetBox = document.querySelector(".tweet-box");
+const tweetBtn = document.querySelector(".tweet-button");
+const visibility = document.querySelector(".visibility");
+tweet.addEventListener("focus", () => {
+  tweet.style.height = "auto";
+  tweet.style.height = tweet.scrollHeight + "px";
+  tweetBox.style.borderBottom = "2px solid #232242";
+  tweetBtn.style.backgroundColor = "#fff";
+  visibility.classList.remove("not-show");
+});
+tweet.addEventListener("blur", () => {
+  visibility.classList.add("not-show");
+});
+// Search Box
+const searchBox = document.querySelector(".search-box");
+const searchInput = document.querySelector(".search-input");
+searchInput.addEventListener("click", () => {
+  searchBox.style.border = "2px solid oklch(54.6% 0.245 262.881) ";
+});
+searchInput.addEventListener("blur", () => {
+  searchBox.style.border = "2px solid oklch(27.9% 0.041 260.031) ";
+});
